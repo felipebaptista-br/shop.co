5b98529933b2c3d81760e0cc5382b912075f8efd
Felipe Baptista <felipe.baptista06@gmail.com>
2024-01-28 20:44:38 -0300

    (Server) create config render deploy:  host: '0.0.0.0'

--- server/server.mjs
+++ server/server.mjs
@@ -45,7 +45,7 @@ fastify.register(commentRoutes)
 fastify.register(productRoutes)
 
 try {
-    await fastify.listen({ port: 5000 })
+    await fastify.listen({ port: 5000, host: '0.0.0.0' })
 } catch (err) {
     fastify.log.error(err)
     process.exit(1)
