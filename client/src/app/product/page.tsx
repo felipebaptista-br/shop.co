2308a8c3500a5dc3d0fdb18543d410b79a02c252
Felipe Baptista <felipe.baptista06@gmail.com>
2024-01-29 00:17:26 -0300

    create folders and api connection

---
+++ client/src/app/product/page.tsx
@@ -0,0 +1,11 @@
+
+import './style.css'
+
+export default function ProductDetail() {
+
+    return (
+        <main className='screen-product-detail'>
+            ProductDetail
+        </main>
+    )
+}
---
+++ client/src/app/showcase/page.tsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+export default function ShowCase() {
+
+    return (
+        <main className='screen-showcase'>
+            ShowCase
+        </main>
+    )
+}
